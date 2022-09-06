517e2ee836971d8e7b553ef4f58d54b29a584d25
Alsou <gothicgirl0102030@gmail.com>
2022-09-06 15:17:01 +0300

    NewProject

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using MyLib;
+
+Figure.CalculateArea(56);
+Figure.CalculateArea(68, 78, 65);
+Figure.isRightTriangle(68, 78, 65);
+Figure.isRightTriangle(3, 4, 5);
+
+Console.WriteLine(Figure.CalculateArea(56));
---
+++ MyApp/MyLib/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+namespace MyLib
+{
+    public class Figure
+    {
+        public static int CalculateArea(int r) => (int)(Math.PI * r * r);
+
+        public static int CalculateArea(int a, int b, int c)
+        {
+            if (a == 0 || b == 0 || c == 0)
+            {
+                return 0;
+            }
+
+            if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                return 0;
+            }
+
+            double p = (a + b + c) / 2;
+
+            return (int)Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static bool isRightTriangle(int a, int b, int c)
+        {
+            return (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b);
+        }
+    }
+
+}
